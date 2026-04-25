d1571aa31a3b6afb5aa53ed3d32828c84de75d79
cxiangood <1504047409@qq.com>
2026-04-25 20:40:29 +0800

    Add domain tokenizer from origin/nmj

--- token_classify/__init__.py
+++ token_classify/__init__.py
@@ -1,3 +1,11 @@
-from token_classify.analyzer import SemanticDensityAnalyzer
+from token_classify.domain_tokenizer import DomainAdaptiveTokenizer, tokenize_text
 
-__all__ = ["SemanticDensityAnalyzer"]
+__all__ = ["SemanticDensityAnalyzer", "DomainAdaptiveTokenizer", "tokenize_text"]
+
+
+def __getattr__(name: str):
+    if name == "SemanticDensityAnalyzer":
+        from token_classify.analyzer import SemanticDensityAnalyzer
+
+        return SemanticDensityAnalyzer
+    raise AttributeError(name)
